bd9ae2a7ae52c97eb20b11a61e818ca5b93e676b
nickruta <nickruta@gmail.com>
2016-08-03 14:40:35 -0400

    Added circularly linked list implementation

--- DataStructuresAlgorithmsCpp/TestCircularlyLinkedList.cpp
+++ DataStructuresAlgorithmsCpp/TestCircularlyLinkedList.cpp
@@ -14,41 +14,48 @@ A circularly linked list has the same kind of nodes as a singly linked list. It 
 using std::cout;
 using std::endl;
 
-typedef std::string Elem;				// element type
-class CircularlyLinkedListNode {					// circularly linked list node
+typedef std::string Elem;                       // element type
+
+class CircularlyLinkedListNode {				// circularly linked list node
 private:
-    Elem elem;					// linked list element value
+    Elem elem;                                  // linked list element value
     CircularlyLinkedListNode* next;				// next item in the list
     
     friend class CircularlyLinkedList;			// provide CircularlyLinkedList access
 };
 
-class CircularlyLinkedList {				// a circularly linked list
+class CircularlyLinkedList {                    // a circularly linked list
 public:
-    CircularlyLinkedList();				// constructor
-    ~CircularlyLinkedList();				// destructor
-    bool empty() const;				// is list empty?
-    const Elem& front() const;			// element at cursor
-    const Elem& back() const;			// element following cursor
-    void advance();				// advance cursor
-    void add(const Elem& e);			// add after cursor
-    void remove();				// remove node after cursor
+    CircularlyLinkedList();                     // constructor
+    ~CircularlyLinkedList();                    // destructor
+    bool empty() const;                         // is list empty?
+    const Elem& front() const;                  // element at cursor
+    const Elem& back() const;                   // element following cursor
+    void advance();                             // advance cursor
+    void add(const Elem& e);                    // add after cursor
+    void remove();                              // remove node after cursor
 private:
-    CircularlyLinkedListNode* cursor;				// the cursor
+    CircularlyLinkedListNode* cursor;
 };
 
-CircularlyLinkedList::CircularlyLinkedList()			// constructor
+// constructor - set the cursor to NULL to create an empty list
+CircularlyLinkedList::CircularlyLinkedList()
 : cursor(NULL) { }
-CircularlyLinkedList::~CircularlyLinkedList()			// destructor
+
+// destructor
+CircularlyLinkedList::~CircularlyLinkedList()
 { while (!empty()) remove(); }
 
-bool CircularlyLinkedList::empty() const		// is list empty?
+bool CircularlyLinkedList::empty() const            // is list empty?
 { return cursor == NULL; }
+
 const Elem& CircularlyLinkedList::back() const		// element at cursor
 { return cursor->elem; }
+
 const Elem& CircularlyLinkedList::front() const		// element following cursor
 { return cursor->next->elem; }
-void CircularlyLinkedList::advance()			// advance cursor
+
+void CircularlyLinkedList::advance()                // advance cursor
 { cursor = cursor->next; }
 
 void CircularlyLinkedList::add(const Elem& e) {		// add after cursor
@@ -56,36 +63,32 @@ void CircularlyLinkedList::add(const Elem& e) {		// add after cursor
     v->elem = e;
     if (cursor == NULL) {			// list is empty?
         v->next = v;				// v points to itself
-        cursor = v;				// cursor points to v
+        cursor = v;                 // cursor points to v
     }
-    else {					// list is nonempty?
-        v->next = cursor->next;			// link in v after cursor
+    else {                          // list is nonempty?
+        v->next = cursor->next;		// link in v after cursor
         cursor->next = v;
     }
 }
 
-void CircularlyLinkedList::remove() {			// remove node after cursor
+void CircularlyLinkedList::remove() {                       // remove node after cursor
     CircularlyLinkedListNode* old = cursor->next;			// the node being removed
-    if (old == cursor) 				// removing the only node?
-        cursor = NULL;				// list is now empty
+    if (old == cursor)                                      // removing the only node?
+        cursor = NULL;                                      // list is now empty
     else
-        cursor->next = old->next;			// link out the old node
-    delete old;					// delete the old node
+        cursor->next = old->next;                           // link out the old node
+    delete old;                                             // delete the old node
 }
 
-// test the singly linked list and print object type
+// test the circularly linked list by creating an MP3 player
 int main() {
-
-    // list of strings
-    CircularlyLinkedList a;
-    a.add("FirstLink");
-
-    // list of strings
-    CircularlyLinkedList b;
-    b.add("AnotherFirstLink");
-
-    cout << typeid(a).name() << endl;
-
-    // successful execution
-    return EXIT_SUCCESS;
+    CircularlyLinkedList playList;		// []
+    playList.add("Stayin Alive");       // [Stayin Alive*]
+    playList.add("Le Freak");           // [Le Freak, Stayin Alive*]
+    playList.add("Jive Talkin");        // [Jive Talkin, Le Freak, Stayin Alive*]
+    
+    playList.advance();                 // [Le Freak, Stayin Alive, Jive Talkin*]
+    playList.advance();                 // [Stayin Alive, Jive Talkin, Le Freak*]
+    playList.remove();                  // [Jive Talkin, Le Freak*]
+    playList.add("Disco Inferno");      // [Disco Inferno, Jive Talkin, Le Freak*]
 }
