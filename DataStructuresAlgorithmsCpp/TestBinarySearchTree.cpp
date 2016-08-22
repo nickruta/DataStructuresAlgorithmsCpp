e9c8dd4daae76a630b336957ede7b5986db5695b
nickruta <nickruta@gmail.com>
2016-08-22 15:52:29 -0400

    Added AVL Tree Implementation.

--- DataStructuresAlgorithmsCpp/TestBinarySearchTree.cpp
+++ DataStructuresAlgorithmsCpp/TestBinarySearchTree.cpp
@@ -1,61 +1,61 @@
+////
+////  TestBinarySearchTree.cpp
+////  DataStructuresAlgorithmsCpp
+////
+////  Created by Nicholas Ruta on 8/15/16.
+////  Based on Data Structures and Algorithm Analysis in C++
+////
 //
-//  TestBinarySearchTree.cpp
-//  DataStructuresAlgorithmsCpp
+//#include <stdio.h>
+//#include <iostream>
+//#include "BinarySearchTree.cpp"
+//using namespace std;
 //
-//  Created by Nicholas Ruta on 8/15/16.
-//  Based on Data Structures and Algorithm Analysis in C++
-//
-
-#include <stdio.h>
-#include <iostream>
-#include "BinarySearchTree.cpp"
-using namespace std;
-
-// Test program
-int main( )
-{
-    BinarySearchTree<int> t;
-    int NUMS = 400000;
-    const int GAP  =   3711;
-    int i;
-    
-    cout << "Checking... (no more output means success)" << endl;
-    
-    for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-        t.insert( i );
-    
-    for( i = 1; i < NUMS; i+= 2 )
-        t.remove( i );
-    
-    if( NUMS < 40 )
-        t.printTree( );
-    if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
-        cout << "FindMin or FindMax error!" << endl;
-    
-    for( i = 2; i < NUMS; i+=2 )
-        if( !t.contains( i ) )
-            cout << "Find error1!" << endl;
-    
-    for( i = 1; i < NUMS; i+=2 )
-    {
-        if( t.contains( i ) )
-            cout << "Find error2!" << endl;
-    }
-    
-    BinarySearchTree<int> t2;
-    t2 = t;
-    
-    for( i = 2; i < NUMS; i+=2 )
-        if( !t2.contains( i ) )
-            cout << "Find error1!" << endl;
-    
-    for( i = 1; i < NUMS; i+=2 )
-    {
-        if( t2.contains( i ) )
-            cout << "Find error2!" << endl;
-    }
-    
-    cout << "Finished testing" << endl;
-    
-    return 0;
-}
+//// Test program
+//int main( )
+//{
+//    BinarySearchTree<int> t;
+//    int NUMS = 400000;
+//    const int GAP  =   3711;
+//    int i;
+//    
+//    cout << "Checking... (no more output means success)" << endl;
+//    
+//    for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+//        t.insert( i );
+//    
+//    for( i = 1; i < NUMS; i+= 2 )
+//        t.remove( i );
+//    
+//    if( NUMS < 40 )
+//        t.printTree( );
+//    if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
+//        cout << "FindMin or FindMax error!" << endl;
+//    
+//    for( i = 2; i < NUMS; i+=2 )
+//        if( !t.contains( i ) )
+//            cout << "Find error1!" << endl;
+//    
+//    for( i = 1; i < NUMS; i+=2 )
+//    {
+//        if( t.contains( i ) )
+//            cout << "Find error2!" << endl;
+//    }
+//    
+//    BinarySearchTree<int> t2;
+//    t2 = t;
+//    
+//    for( i = 2; i < NUMS; i+=2 )
+//        if( !t2.contains( i ) )
+//            cout << "Find error1!" << endl;
+//    
+//    for( i = 1; i < NUMS; i+=2 )
+//    {
+//        if( t2.contains( i ) )
+//            cout << "Find error2!" << endl;
+//    }
+//    
+//    cout << "Finished testing" << endl;
+//    
+//    return 0;
+//}
